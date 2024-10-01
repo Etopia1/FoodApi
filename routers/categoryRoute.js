104007312e188d8c60a32051beaad532a892afac
Etopia1 <jolaetopia81@gmail.com>
2024-10-01 10:06:21 +0100

    New Commit now right now

--- routers/categoryRoute.js
+++ routers/categoryRoute.js
@@ -7,15 +7,15 @@ const {
     deleteCategory
 } = require("../controllers/categoryController");
 const router = express.Router();
-const { authenticate} = require("../middlewares/Auth")
+const { authMiddleware } = require("../middlewares/categoryAuth")
 
 // Public Routes
 router.get('/category', getAllCategories); // Get all categories (public)
 router.get('/category/:categoryId', getCategory); // Get one category (public)
 
 // Protected Routes (Require Authentication)
-router.post('/category/:merchantId', createCategory); // Create a category (auth required)
-router.put('/category/:categoryId', authenticate, updateCategory); // Update a category (auth required)
-router.delete('/category/:categoryId', authenticate, deleteCategory); // Delete a category (auth required)
+router.post('/category/:merchantId', authMiddleware, createCategory); // Create a category (auth required)
+router.put('/category/:categoryId', authMiddleware, updateCategory); // Update a category (auth required)
+router.delete('/category/:categoryId', authMiddleware, deleteCategory); // Delete a category (auth required)
 
 module.exports = router;
