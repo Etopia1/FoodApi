cff12774eb1234edbda61a196ff6709b24348ce1
Etopia1 <jolaetopia81@gmail.com>
2024-10-01 09:57:55 +0100

    New Commit now right

--- routers/categoryRoute.js
+++ routers/categoryRoute.js
@@ -14,7 +14,7 @@ router.get('/category', getAllCategories); // Get all categories (public)
 router.get('/category/:categoryId', getCategory); // Get one category (public)
 
 // Protected Routes (Require Authentication)
-router.post('/category/:merchantId', authenticate, createCategory); // Create a category (auth required)
+router.post('/category/:merchantId', createCategory); // Create a category (auth required)
 router.put('/category/:categoryId', authenticate, updateCategory); // Update a category (auth required)
 router.delete('/category/:categoryId', authenticate, deleteCategory); // Delete a category (auth required)
 
